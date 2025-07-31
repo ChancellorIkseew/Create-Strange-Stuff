787596e418e77666bf27c383ee7a48b2e110358f
ChancellorIkseew <ikse.ksini@bk.ru>
2025-07-31 18:09:58 +1000

    electric drill add energy waste

--- src/main/java/com/strangeStuff/items/ElectricDrill.java
+++ src/main/java/com/strangeStuff/items/ElectricDrill.java
@@ -1,13 +1,22 @@
 package com.strangeStuff.items;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.SlotAccess;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ClickAction;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tiers;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+
 import static com.strangeStuff.items.util.ActionsJoiner.DRILL_ACTIONS;
 
 public class ElectricDrill extends DiggerItem {
@@ -22,8 +31,34 @@ public class ElectricDrill extends DiggerItem {
     }
 
     @Override
-    public final boolean mineBlock(ItemStack stack, Level level, BlockState state, BlockPos pos, LivingEntity player) {
+    public final boolean mineBlock(ItemStack stack, Level level, BlockState state, BlockPos pos, LivingEntity miner) {
+        if (!(miner instanceof Player))
+            return false;
+        Player player = (Player) miner;
+        ItemStack battery = findBattery(player);
+        useBattery(player, battery);
         return true;
+    }
+
+    @Override
+    public boolean canAttackBlock(BlockState p_41441_, Level p_41442_, BlockPos p_41443_, Player player) {
+        return !findBattery(player).isEmpty();
+    }
+
+    private static ItemStack findBattery(@NotNull Player player) {
+        for (ItemStack stack : player.getInventory().items) {
+            if (stack.getItem() instanceof Battery)
+                return stack;
+        }
+        return ItemStack.EMPTY;
+    }
+
+    private static void useBattery(Player player, ItemStack battery) {
+        battery.setDamageValue(battery.getDamageValue() + 1);
+        if (battery.getDamageValue() < battery.getMaxDamage())
+            return;
+        battery.shrink(1);
+        player.playSound(SoundEvents.ITEM_BREAK, 1.0F, 0.5F);
     }
 
     @Override
